dbb32ca4fff1b7cedcfbf5e218cd38e585654f55
Eugeny Pankov <john.pankov@gmail.com>
2012-05-04 02:23:04 -0400

    Sturm-newton lab

--- 4 semester/CMA/5/main.py
+++ 4 semester/CMA/5/main.py
@@ -1,5 +1,6 @@
 from sympy import *
 from sympy.core.mod import *
+import time
 
 EPS = 0.000001
 
@@ -9,6 +10,11 @@ c = -70.9238
 
 x = Symbol('x')
 fx = x**3 + a*x**2 + b*x + c
+
+L = -10.0
+R = 20.0
+
+print "F(x):", 	fx
 #fx = x
 
 _sturm_cache = {}
@@ -60,16 +66,74 @@ def isolate_next_root(fx, l, r):
 	return r
 
 
+def compactize_interval(fx, i, d):
+	l,r = i
+	while r - l > d:
+		if count_roots_between(fx, l, (l+r)/2) == 1:
+			r = (l+r)/2
+		else:
+			l = (l+r)/2
+	return [l,r]
+
 def split_roots(fx, l, r):
 	res = []
 	i = l
 	while i < r:
 		l = i
 		i = isolate_next_root(fx, i, r)
-		res += [[l,i]]
+		interval = [l,i]
+		interval = compactize_interval(fx, interval, 1)
+		res += [interval]
 	return res
 
 
+def get_arc_start(fx, l, r):
+	x = r
+	if fx.subs('x', x) * diff(diff(fx)).subs('x', x) > 0:
+		return 'r'
+	x = l
+	if fx.subs('x', x) * diff(diff(fx)).subs('x', x) > 0:
+		return 'l'
 
-print solve(fx)
-print split_roots(fx, -100, 100)
+
+def solve_horde(fx, l, r):
+	x0 = eval(get_arc_start(fx, l, r))
+	x = r if (x0 == l) else l
+	xl = 999
+	itc = 0
+	while abs(xl-x) > EPS:
+		itc += 1
+		xl = x
+		x = x - fx.subs('x', x) * (x0 - x) / (fx.subs('x', x0) - fx.subs('x', x))
+	return x, itc
+
+
+def solve_newton(fx, l, r):
+	df = diff(fx)
+	x = eval(get_arc_start(fx, l, r))
+	xl = 999
+	itc = 0
+	while abs(xl-x) > EPS:
+		itc += 1
+		xl = x
+		x = x - fx.subs('x', x) / df.subs('x', x)
+	return x, itc
+
+print 'Exact solution:', solve(fx)
+intervals = split_roots(fx, L, R)
+print 'Isolated roots:', intervals
+
+print '--- Hordes'
+idx = 1
+for i in intervals:
+	r, i = solve_horde(fx, i[0], i[1])
+	print 'Root %i: %.4f (%i iterations)' % (idx, r, i)
+	idx += 1
+
+
+print '--- Newton'
+idx = 1
+for i in intervals:
+	r, i = solve_newton(fx, i[0], i[1])
+	print 'Root %i: %.4f (%i iterations)' % (idx, r, i)
+	idx += 1
