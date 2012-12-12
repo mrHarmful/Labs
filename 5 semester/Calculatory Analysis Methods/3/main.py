8ec46f96597262d02a8d8dff7774454503838f87
Eugeny Pankov <e@ajenti.org>
2012-12-12 20:37:43 +0300

    CMA lab4

--- 5 semester/Calculatory Analysis Methods/3/main.py
+++ 5 semester/Calculatory Analysis Methods/3/main.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import math
+import time
 
-from sympy import diff
+from sympy import *
 from sympy.abc import x
 from sympy.core import Symbol
 from sympy.solvers import solve
@@ -9,49 +10,139 @@ from sympy.solvers import solve
 xmin = -1.0
 xmax = 1.0
 
-a = math.sin(2)
-b = math.cos(2)
-a=1;b=1
+a = math.sin(5)
+b = math.cos(5)
+#a=1;b=1
 
+
+def mkbasis(steps):
+    basis = [(x ** (i*2-2) * (1 - x ** 2)) for i in range(0, steps + 1)]
+    basis[0] = 0
+    return basis
+
+def mkxs(steps):
+    return [xmin + (xmax - xmin) / steps * i for i in range(0, steps + 1)]
 
 def colocations(steps):
-    basis = [(x ** i * (1 - x ** 2)) for i in range(0, steps + 1)]
-    basis[0] = 0
-
-    xs = [xmin + (xmax - xmin) / steps * i for i in range(0, steps + 1)]
+    basis = mkbasis(steps)   
+    xs = mkxs(steps)
     ax = [1] + [Symbol('a%i' % i) for i in range(1, steps + 1)]
 
     yapprox = sum(ax[i] * basis[i] for i in range(0, steps + 1))
     error = a * diff(diff(yapprox, 'x'), 'x') + (1 + b * x ** 2) * yapprox + 1
 
-    matrix = [error.subs(x, xs[i]) for i in range(0, steps )]
-
+    matrix = [error.subs(x, xs[i]) for i in range(0, steps)]
+    #matrix += [yapprox.subs(x, xmin)]
+    #matrix += [yapprox.subs(x, xmax)]
+    #print '---------------'
+    #print 'xs', xs
+    #print xmin, xmax, steps
+    #print 'matrix', matrix
     sol = solve(matrix)
+    #print 'sol', sol
     y = yapprox.subs(sol)
-    print sol
     return y
 
 
-def plot_colocations(steps, ga, **kwargs):
-    y = colocations(steps)
+def mnk_int(steps):
+    basis = mkbasis(steps)   
+    xs = mkxs(steps)
+    ax = [1] + [Symbol('a%i' % i) for i in range(1, steps + 1)]
+
+    yapprox = sum(ax[i] * basis[i] for i in range(0, steps + 1))
+    error = a * diff(diff(yapprox, 'x'), 'x') + (1 + b * x ** 2) * yapprox + 1
+
+    #print '---------------'
+    ediffs = [integrate(error * diff(error, ax[i]), (x, xmin, xmax)) for i in range(1, steps + 1)]
+    #print ediffs
+    sol = solve(ediffs)
+    #print sol
+    y = yapprox.subs(sol)
+    return y
+
+
+def mnk_dis(steps):
+    basis = mkbasis(steps)
+    xs = mkxs(steps)
+    ax = [1] + [Symbol('a%i' % i) for i in range(1, steps + 1)]
+
+    yapprox = sum(ax[i] * basis[i] for i in range(0, steps + 1))
+    error = a * diff(diff(yapprox, 'x'), 'x') + (1 + b * x ** 2) * yapprox + 1
+
+    #print '---------------'
+    ediffs = [
+        sum((error * diff(error, ax[i])).subs(x, xi) for xi in xs)
+        for i in range(1, steps + 1)
+    ]
+
+    #print ediffs
+    sol = solve(ediffs)
+    #print sol
+    y = yapprox.subs(sol)
+    return y
+
+
+def galerkin(steps):
+    basis = mkbasis(steps)
+    xs = mkxs(steps)
+    ax = [1] + [Symbol('a%i' % i) for i in range(1, steps + 1)]
+
+    yapprox = sum(ax[i] * basis[i] for i in range(0, steps + 1))
+    error = a * diff(diff(yapprox, 'x'), 'x') + (1 + b * x ** 2) * yapprox + 1
+
+    ediffs = [integrate(error * basis[i], (x, xmin, xmax)) for i in range(1, steps + 1)]
+    #print ediffs
+    sol = solve(ediffs)
+    #print sol
+    y = yapprox.subs(sol)
+    return y
+
+def plot(fx, steps, ga, **kwargs):
+    t0 = time.time()
+    y = fx(steps)
+    dt = time.time() - t0
+
     gsteps = 100
-    print y
     xs = [xmin + (xmax - xmin) / gsteps * i for i in range(0, gsteps + 1)]
     ys = [y.subs(x, xi) for xi in xs]
+
+    error = a * diff(diff(y, 'x'), 'x') + (1 + b * x ** 2) * y + 1
+    #emax = max(abs(error.subs(x, xi)) for xi in xs)
+    eavg = sum(abs(error.subs(x, xi)) for xi in xs) / len(xs)
+    print '-- %12s @ %2i steps: err = %.10f, time = %.5fs' % (fx.__name__, steps, eavg, dt)
     ga.plot(xs, ys, **kwargs)
 
 
 fig = plt.figure()
 
-ga = fig.add_subplot(111)
+print 
+ga = fig.add_subplot(221)
 ga.grid()
 
+plot(colocations, 2, ga, color='red')
+plot(colocations, 5, ga, color='green')
 
-plot_colocations(3, ga, color='red')
-plot_colocations(5, ga, color='green')
-plot_colocations(20, ga, color='blue')
+print 
+ga = fig.add_subplot(222)
+ga.grid()
 
-#ga.plot(*plot_euler(step_rounge_coutte), color='green')
+plot(mnk_int, 2, ga, color='red')
+plot(mnk_int, 5, ga, color='green')
+
+print 
+ga = fig.add_subplot(223)
+ga.grid()
+
+plot(mnk_dis, 2, ga, color='red')
+plot(mnk_dis, 5, ga, color='green')
+
+
+print 
+ga = fig.add_subplot(224)
+ga.grid()
+
+plot(galerkin, 2, ga, color='red')
+plot(galerkin, 5, ga, color='green')
 
 
 fig.show()
